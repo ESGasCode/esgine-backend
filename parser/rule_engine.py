0a9fa712fe18a72c545828b49205e346a9fedbf2
Isaiah Owolabi <io@esgascode.com>
2025-06-24 16:25:51 +0100

    Update rule_engine.py

--- parser/rule_engine.py
+++ parser/rule_engine.py
@@ -1,4 +1,3 @@
-import yaml
 import json
 
 def get_nested_value(data, dotted_key):
@@ -56,20 +55,3 @@ def validate(report, rules):
 
 def run_rule_engine(data, rules):
     return validate(data, rules)
-
-def load_rule(regulator: str, base_path="parser/rules/unified_esg_rules.yaml"):
-    """
-    Load rule set by regulator prefix (e.g., FCA, SEC, SFDR, ISSB).
-    """
-    with open(base_path, "r") as file:
-        all_rules = yaml.safe_load(file)
-
-    regulator_rules = [
-        rule for rule in all_rules
-        if rule.get("regulation", "").lower().startswith(regulator.lower())
-    ]
-
-    return {
-        "regulation": regulator.upper(),
-        "compliance_check": regulator_rules
-    }
